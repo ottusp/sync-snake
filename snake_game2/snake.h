94bc24fbf725f415377a8350bcb3e4f545ef39ab
laisdeghaide <laisdeghaide@gmail.com>
2021-12-13 11:03:25 -0300

    tentando colocar mais uma cobrinha

--- snake_game2/snake.h
+++ snake_game2/snake.h
@@ -6,19 +6,38 @@
 #ifndef SNAKE_GAME_H
 #define SNAKE_GAME_H
 
+/*enum Direction {
+  up,
+  down,
+  left, 
+  right,
+  A,
+  W,
+  S,
+  D
+};*/
+
 struct CharPosition {
 	int x, y;
 	CharPosition(int col, int row);
 	CharPosition();
 };
 
+struct Player {
+	std::vector<CharPosition> snake;
+	char snakeBody;
+	char direction;
+	int score;
+	bool bEatsFruit;
+};
+
 class snakeGame {
 private:
-	int score, del, maxwidth, maxheight;
+	int del, maxwidth, maxheight; //score
 	char direction, snakeBody, edgechar, fruitchar;
-	bool bEatsFruit;
+	//bool bEatsFruit;
 	CharPosition fruit;
-	std::vector<CharPosition> snake;
+	//std::vector<CharPosition> snake;
 
 	void InitGameWindow();
 	void DrawWindow();  
